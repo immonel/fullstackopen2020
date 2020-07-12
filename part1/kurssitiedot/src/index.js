b7cc0b4916bf2c476d088dfab264035c14c4f6fb
immonel2 <leo.immonen@aalto.fi>
2020-07-13 01:26:21 +0300

    Fixed declarations

--- part1/kurssitiedot/src/index.js
+++ part1/kurssitiedot/src/index.js
@@ -20,43 +20,43 @@ const App = () => {
     ]
   }
 
-  const Header = (props) => {
-    return (
-    <h1>{props.course.name}</h1>
-    )
-  }
-
-  const Content = (props) => {
-    return (
-      <div>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} /> 
-      </div>
-    )
-  }
-
-  const Part = (props) => {
-    return (
-      <>
-      <p>{props.part.name} {props.part.exercises}</p>
-      </>
-    )
-  }
-
-  const Total = (props) => {
-    return (
-    <p>Number of exercises {props.parts.map(i => i.exercises)
-                                       .reduce((i, j) => i + j)}</p>
-    )
-  }
-
   return (
     <div>
       <Header course={course} />
       <Content parts={course.parts}/>
       <Total parts={course.parts} />
     </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+  <h1>{props.course.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} /> 
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+    <p>{props.part.name} {props.part.exercises}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+  <p>Number of exercises {props.parts.map(i => i.exercises)
+                                     .reduce((i, j) => i + j)}</p>
   )
 }
 
